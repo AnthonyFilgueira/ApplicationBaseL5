d69b7bf925e68348bd1694c0e2a60d7355487dbd
Anthony Filgueira <anthonyfilgueira@hotmail.com>
2017-10-11 13:39:42 -0400

    cambiar conexion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <script src="{{ asset('/js/jquery-1.11.3.min.js') }}"></script>
         @yield('estilos')
 	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<!-- <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'> -->
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
